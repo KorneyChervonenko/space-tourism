2f03eddc351fd7b1813573253f88a7eee94c231c
Korney Chervonenko <pokryshkinkorney@gmail.com>
2025-01-04 20:02:13 +0200

    add div promo

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,16 +6,18 @@ export default function Homepage() {
 
 	return (
 		<section className={styles.homePage}>
-			{/* <h2>Homepage</h2> */}
-			<h2 className={styles.heading}>
-				SO, YOU WANT TO TRAVEL TO <br />
-				<span className={styles.headingBigWord}>SPACE</span>
-			</h2>
-			<p className={styles.promoText}>
-				Let’s face it; if you want to go to space, you might as well genuinely go to outer space and
-				not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly
-				out of this world experience!
-			</p>
+			<h2 className="visually-hidden">Homepage</h2>
+			<div className={styles.promo}>
+				<h3 className={styles.heading}>
+					SO, YOU WANT TO TRAVEL TO <br />
+					<span className={styles.headingBigWord}>SPACE</span>
+				</h3>
+				<p className={styles.promoText}>
+					Let’s face it; if you want to go to space, you might as well genuinely go to outer space
+					and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a
+					truly out of this world experience!
+				</p>
+			</div>
 			<button className={styles.exploreBtn} type="button" onClick={() => navigate('/destination')}>
 				EXPLORE
 			</button>
