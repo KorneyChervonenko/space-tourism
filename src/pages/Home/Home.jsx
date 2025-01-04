c44d6a0818071a4ee7fb41f2ae4161b80aae1cf3
Korney Chervonenko <pokryshkinkorney@gmail.com>
2025-01-04 17:14:05 +0200

    define font styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -16,7 +16,7 @@ export default function Homepage() {
 				not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly
 				out of this world experience!
 			</p>
-			<button className="exploreBtn" type="button" onClick={() => navigate('/destination')}>
+			<button className={styles.exploreBtn} type="button" onClick={() => navigate('/destination')}>
 				EXPLORE
 			</button>
 		</section>
