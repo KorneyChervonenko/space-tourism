26f2df084789e30a77783dce09d2873cc9271d16
Korney Chervonenko <pokryshkinkorney@gmail.com>
2025-01-04 14:59:59 +0200

    add Barlow font

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,11 +7,11 @@ export default function Homepage() {
 	return (
 		<section className={styles.homePage}>
 			{/* <h2>Homepage</h2> */}
-			<h2 className="heading">
+			<h2 className={styles.heading}>
 				SO, YOU WANT TO TRAVEL TO <br />
-				<span className="headingBigWord">SPACE</span>
+				<span className={styles.headingBigWord}>SPACE</span>
 			</h2>
-			<p className="promoText">
+			<p className={styles.promoText}>
 				Let’s face it; if you want to go to space, you might as well genuinely go to outer space and
 				not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly
 				out of this world experience!
