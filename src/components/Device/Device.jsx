d3ed1744005fce2149534bb41b8ee513a0ff93c2
Korney Chervonenko <pokryshkinkorney@gmail.com>
2025-01-09 22:57:32 +0200

    Technology draft ready

--- src/components/Device/Device.jsx
+++ src/components/Device/Device.jsx
@@ -7,24 +7,40 @@ Device.propTypes = {
 };
 
 export default function Device({ device }) {
-	const desktopWidth = getComputedStyle(document.documentElement).getPropertyValue(
-		'--desktop-width'
-	);
-	// console.log(desktopWidth, Number(desktopWidth));
+	// const desktopWidth = getComputedStyle(document.documentElement).getPropertyValue(
+	// 	'--desktop-width'
+	// );
+	// // const desktopWidth = '90rem';
+	// const isDesktop = window.matchMedia(`(min-width: ${desktopWidth})`).matches;
+	// // console.log(desktopWidth, isDesktop);
+	// // console.log(window.matchMedia(`(max-width: ${desktopWidth})`).matches);
 
 	const imagePath = '../src/assets/technology/';
 	const imgPortrait = imagePath + device.images.portrait.split('/').at(-1);
 	const imgLandscape = imagePath + device.images.landscape.split('/').at(-1);
 
+	// const illustrationBGStyle = {
+	// 	backgroundRepeat: 'no-repeat',
+	// 	backgroundPosition: 'center',
+	// 	backgroundSize: 'contain',
+	// 	backgroundImage: `url("${isDesktop ? imgPortrait : imgLandscape}")`,
+	// };
+
+	// console.log(illustrationBGStyle);
+
 	return (
 		<section className={styles.device}>
+			{/* <div className={styles.illustration}></div> */}
 			<picture className={styles.deviceImage}>
-				{/* <source srcSet={imgWebp} type="image/webp" /> */}
-				<source srcSet={imgPortrait} media={`(min-width: ${desktopWidth})`}></source>
-				<source srcSet={imgLandscape} media={`(max-width: ${desktopWidth})`}></source>
+				<source srcSet={imgPortrait} media="(min-width: 90rem)"></source>
+				<source srcSet={imgLandscape} media="(max-width: 89.99rem)"></source>
 				<img src={imgLandscape} alt={device.name} />
 			</picture>
-			<h3>{device.name}</h3>
+			<h3 className={styles.deviceHeading}>
+				<div className={styles.deviceSubHeader}>THE TERMINOLOGY...</div>
+				<div className={styles.deviceName}>{device.name.toUpperCase()}</div>
+			</h3>
+			<div className={styles.deviceDescription}>{device.description}</div>
 		</section>
 	);
 }
